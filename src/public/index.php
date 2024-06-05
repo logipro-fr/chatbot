cd01c46f6385fcaca819281c7fa524ae5b1bf189
Marine-Bernard <mjrmbernard@gmail.com>
2024-06-05 11:17:18 +0200

    test commit

--- src/public/index.php
+++ src/public/index.php
@@ -5,4 +5,5 @@ require_once dirname(__DIR__, 2) . '/vendor/autoload_runtime.php';
 return function (array $context) {
     $kernelClass = $_ENV['KERNEL_CLASS'];
     return new $kernelClass($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    ?> test<?php
 };
