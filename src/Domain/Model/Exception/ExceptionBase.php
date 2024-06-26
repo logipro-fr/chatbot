082b0bc2a354522197301a6d2320a3841a56c458
Marine-Bernard <mjrmbernard@gmail.com>
2024-06-26 11:20:40 +0200

    Creéation File Exception Base

---
+++ src/Domain/Model/Exception/ExceptionBase.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Chatbot\Domain\Model\Exceptions;
+
+use Exception;
+
+
+class BaseException extends Exception
+{
+    protected string $type = "exception";
+
+    public function __construct(
+        string $message = "",
+        int $code = 0,
+        private mixed $data = null,
+        ?\Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getData(): mixed
+    {
+        return $this->data;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+}
